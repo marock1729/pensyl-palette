1ccbf6643645cfbda15c2e442e4e5171c758202c
marock <marock.tokyo@gmail.com>
2024-11-10 00:42:33 +0900

    リンク機能の不要な記述を削除

--- src/components/plugins/TiptapLinkExtension.ts
+++ src/components/plugins/TiptapLinkExtension.ts
@@ -4,85 +4,22 @@ import {
   mergeAttributes,
   PasteRuleMatch,
 } from "@tiptap/core";
-import { find, registerCustomProtocol, reset } from "linkifyjs";
-
-export interface LinkProtocolOptions {
-  /**
-   * The protocol scheme to be registered.
-   * @default '''
-   * @example 'ftp'
-   * @example 'git'
-   */
-  scheme: string;
-
-  /**
-   * If enabled, it allows optional slashes after the protocol.
-   * @default false
-   * @example true
-   */
-  optionalSlashes?: boolean;
-}
+import { find, reset } from "linkifyjs";
 
 export interface LinkOptions {
-  /**
-   * If enabled, the extension will automatically add links as you type.
-   * @default true
-   * @example false
-   */
-  autolink: boolean;
-
-  /**
-   * An array of custom protocols to be registered with linkifyjs.
-   * @default []
-   * @example ['ftp', 'git']
-   */
-  protocols: Array<LinkProtocolOptions | string>;
-
-  /**
-   * Default protocol to use when no protocol is specified.
-   * @default 'http'
-   */
-  defaultProtocol: string;
-  /**
-   * Adds a link to the current selection if the pasted content only contains an url.
-   * @default true
-   * @example false
-   */
   linkOnPaste: boolean;
-
-  /**
-   * HTML attributes to add to the link element.
-   * @default {}
-   * @example { class: 'foo' }
-   */
   HTMLAttributes: Record<string, any>;
-
-  /**
-   * A validation function that modifies link verification for the auto linker.
-   * @param url - The url to be validated.
-   * @returns - True if the url is valid, false otherwise.
-   */
-  validate: (url: string) => boolean;
 }
 
 declare module "@tiptap/core" {
   interface Commands<ReturnType> {
     link: {
-      /**
-       * Set a link mark
-       * @param attributes The link attributes
-       * @example editor.commands.setLink({ href: 'https://tiptap.dev' })
-       */
       setLink: (attributes: {
         href: string;
         target?: string | null;
         rel?: string | null;
         class?: string | null;
       }) => ReturnType;
-      /**
-       * Unset a link mark
-       * @example editor.commands.unsetLink()
-       */
       unsetLink: () => ReturnType;
     };
   }
@@ -90,43 +27,20 @@ declare module "@tiptap/core" {
 
 export const linkExtension = Mark.create<LinkOptions>({
   name: "link",
-
   priority: 1000,
-
   keepOnSplit: false,
-
   exitable: true,
-
-  onCreate() {
-    this.options.protocols.forEach((protocol) => {
-      if (typeof protocol === "string") {
-        registerCustomProtocol(protocol);
-        return;
-      }
-      registerCustomProtocol(protocol.scheme, protocol.optionalSlashes);
-    });
-  },
-
   onDestroy() {
     reset();
   },
-
-  inclusive() {
-    return this.options.autolink;
-  },
-
   addOptions() {
     return {
       linkOnPaste: true,
-      autolink: true,
-      protocols: [],
-      defaultProtocol: "http",
       HTMLAttributes: {
         target: "_self",
         rel: "noopener noreferrer nofollow",
         class: null,
       },
-      validate: (url) => !!url,
     };
   },
 
@@ -157,7 +71,6 @@ export const linkExtension = Mark.create<LinkOptions>({
         getAttrs: (dom) => {
           const href = (dom as HTMLElement).getAttribute("href");
 
-          // prevent XSS attacks
           if (!href) {
             return false;
           }
@@ -204,10 +117,7 @@ export const linkExtension = Mark.create<LinkOptions>({
           const foundLinks: PasteRuleMatch[] = [];
 
           if (text) {
-            const { validate } = this.options;
-            const links = find(text).filter(
-              (item) => item.isLink && validate(item.value)
-            );
+            const links = find(text).filter((item) => item.isLink);
 
             if (links.length) {
               links.forEach((link) =>
